b8041957e1ee80689cd3973be4c58f59a24806df
LokeshReddyRatnala <lokeshreddyratnala86@gmail.com>
2025-04-25 22:20:13 -0500

    Added the folder

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hi ,this is lokesh Reddy
+      </header>
+    </div>
+  );
+}
+
+export default App;
